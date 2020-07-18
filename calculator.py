e77cb568b05b72b3c757a6861678663a36bc5160
Ramesh Kumar <50226667+inforkgodara@users.noreply.github.com>
2020-07-18 19:11:30 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -27,7 +27,7 @@ print("4. Divide")
 while True:
     # Take input from the console
     choice = input("Enter choice(1/2/3/4 or n to cancel): ")
-    # Check if choice is one of the four options
+    # Check if choice is one of the five options
     if choice in ('1', '2', '3', '4'):
         firstNumber = float(input("Enter first number: "))
         secondNumber = float(input("Enter second number: "))
